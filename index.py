58b9cf0a1bfb9e89492e512ec3a779809ed78f8d
李泳 <neilli1992@pc-199-18.customer.liv.liberty.lan>
2015-02-04 20:43:32 +0000

    change icon

--- index.py
+++ index.py
@@ -9,7 +9,9 @@ def hello():
     return "Hello, world! - Flask\n"
 
 if __name__ == "__main__":
+    # 本地测试环境
     app.run()
 else:
+    # BAE发布环境
     from bae.core.wsgi import WSGIApplication
     application = WSGIApplication(app)
