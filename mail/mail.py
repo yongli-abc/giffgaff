d087e4fb30562e3d405425535b0d7158503a96f2
李泳 <yong.li1992@foxmail.com>
2015-02-13 10:06:09 +0000

    mailgun独立域名发件被qq邮箱屏蔽。切换到sendCloud服务。

--- mail/mail.py
+++ mail/mail.py
@@ -1,19 +1,39 @@
 # --*-- coding:utf8 --*--
 import requests
 
-url = "https://api.mailgun.net/v2/sandboxca42fed017ca4ec7ae250e36e7560562.mailgun.org/messages"
-api = "key-3d46b25039688ecfd3c36dd68e660395"
-sender = "spread_giffgaff@yongli1992.com"
+# url = "https://api.mailgun.net/v2/sandboxca42fed017ca4ec7ae250e36e7560562.mailgun.org/messages"
+# api = "key-3d46b25039688ecfd3c36dd68e660395"
+# sender = "spread_giffgaff@yongli1992.com"
+#
+# def send_email(receiver_list, subject, text):
+#     for receiver, name in receiver_list:
+#         try:
+#             requests.post(url, auth=("api", api), data={
+#                 "from":"giffgaff 分享 <%s>" % sender,
+#                 "to": "%s <%s>" % (name,receiver),
+#                 "subject": subject,
+#                 "text": text
+#             })
+#             print "Send successfully."
+#         except:
+#             print "Error: unable to send email"
+#
+# # Sendcloud发件模块
+url="http://sendcloud.sohu.com/webapi/mail.send.json"
 
 def send_email(receiver_list, subject, text):
     for receiver, name in receiver_list:
+        params = {"api_user": "giffgaff", \
+            "api_key" : "KVYxzpw5S44nf5e3",\
+            "to" : "%s" % receiver, \
+            "from" : "giffgaff@yongli1992.com", \
+            "fromname" : "giffgaff 分享", \
+            "subject" : "%s" % subject, \
+            "html": "%s" % text \
+        }
+
         try:
-            requests.post(url, auth=("api", api), data={
-                "from":"giffgaff 分享 <%s>" % sender,
-                "to": "%s <%s>" % (name,receiver),
-                "subject": subject,
-                "text": text
-            })
+            requests.post(url, files="", data=params)
             print "Send successfully."
         except:
-            print "Error: unable to send email"
+            print "Error: unable to send email!"
