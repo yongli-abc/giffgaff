9919aac53f9cb53de27cca6967ccb32b7d477b1c
李泳 <yong.li1992@foxmail.com>
2015-02-15 16:02:42 +0000

    test log

--- index.py
+++ index.py
@@ -57,7 +57,7 @@ class OrderForm(Form):
                     field.errors.append(u"该邮箱已经申请过，请勿重复提交")
                     # 记录重复提交行为
 
-                    logger.debug("用户 %s 尝试重复提交。" % field.data)
+                    logger.debug("用户尝试重复提交。")
                     return False
             except Exception as e:
                 field.errors.append(str(e))
