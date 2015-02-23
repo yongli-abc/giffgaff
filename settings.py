59bc587614ac2ac15916d6e669f651e2dc21ba12
李泳 <yong.li1992@foxmail.com>
2015-02-23 14:23:44 +0000

    更改电话验证的正则模式

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ DATABASE = "tmp/data.sqlite"
 ADMIN = "admin"
 PASSWORD = "spreadgiffgaff"
 EMAIL_PATTERN = '^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$'
-PHONE_PATTERN = '(1)(3\d|4[5,7]|5[0-3,5-9]|8[0,2,3,6-9])\D*(\d{4})\D*(\d{4})$'
+PHONE_PATTERN = '(1)(3\d|4\d|5\d|7\d|8\d)\D*(\d{4})\D*(\d{4})$'
 DEBUG = True
